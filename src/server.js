b00ca0fec443bbc82c89c59c3381d1e2399e4a7a
Adham <muzaffarov.adham@gmail.com>
2021-07-05 11:57:43 +0500

    search backend ended

--- src/server.js
+++ src/server.js
@@ -27,5 +27,56 @@ app.get('/films', async (req,res) => {
     res.status(200).json(users)
 })
 
+app.post('/api/users/search', async (req,res) => {
+    const {search} = req.body
+    const query = `
+    SELECT * FROM users
+    WHERE first_name ILIKE '%${search}%'
+    OR last_name ILIKE '%${search}%'
+    OR contact ILIKE '%${search}%'
+    `
+    const results = await fetch(query)
+    if(results){
+        res.status(200).json(results)
+    } else {
+        res.status(404).json({message: "Not Found"})
+    }
+    
+})
+
+app.post('/api/books/search', async (req,res) => {
+    const {search} = req.body
+    const query = `
+    SELECT * FROM books
+    WHERE name ILIKE '%${search}%'
+    OR date ILIKE '%${search}%'
+    OR category ILIKE '%${search}%'
+    `
+    const results = await fetch(query)
+    if(results){
+        res.status(200).json(results)
+    } else {
+        res.status(404).json({message: "Not Found"})
+    }
+    
+})
+
+app.post('/api/films/search', async (req,res) => {
+    const {search} = req.body
+    const query = `
+    SELECT * FROM films
+    WHERE name ILIKE '%${search}%'
+    OR date ILIKE '%${search}%'
+    OR category ILIKE '%${search}%'
+    `
+    const results = await fetch(query)
+    if(results){
+        res.status(200).json(results)
+    } else {
+        res.status(404).json({message: "Not Found"})
+    }
+    
+})
+
 
 app.listen(8000, () => console.log('http://localhost:8000'))
